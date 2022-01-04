b354c1c9e506f5477b7a6fc93c4bf62aed18eace
rdghosal <rdghosal@gmail.com>
2022-01-04 12:46:11 -0500

    Updated Landing styling

--- client/src/Landing.tsx
+++ client/src/Landing.tsx
@@ -7,12 +7,15 @@ const Landing = (props: RouterProps) => {
 	return (
 		<div className="landing">
 			<div className="landing__banner">
-				<div className="landing__logo"></div>
-				<p className="landing__text">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, quis!
-				</p>
-				<button className="landing__btn btn"
-					onClick={ () => history.push("/app") }>Enter</button>
+				<div className="landing__container">
+					<div className="landing__logo"></div>
+					<p className="landing__title">relax.</p>
+					<p className="landing__text">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, quis!
+					</p>
+					<button className="landing__btn btn"
+						onClick={ () => history.push("/app") }>Enter</button>
+				</div>
 			</div>
 		</div>
 	);
